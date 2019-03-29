36f210a7503b7092d957d7b0065540a2f2d3ad62
root <root@octopi.ml>
2019-03-29 19:31:41 +0000

    formatting

--- nginx/start.sh
+++ nginx/start.sh
@@ -9,28 +9,28 @@ CERT_PATH=/etc/letsencrypt/live/${DOMAIN}
 mkdir -p "${CERT_PATH}"
 cd "${CERT_PATH}"
 
-	
+
 EXPIRES_TODAY="openssl x509 -checkend 86400 -noout -in fullchain.pem"
 if [ ! -f fullchain.pem ] || [ "$(eval $EXPIRES_TODAY)" = "1" ]; then
-    touch privkey.pem
-    touch cert.pem
-    touch fullchain.pem
-	
-	envsubst '${DOMAIN} ${ADMIN_EMAIL}' < /usr/src/app/cert.cnf.template > /usr/src/app/cert.cnf
-	
-	cat /usr/src/app/cert.cnf
-	
-    openssl req \
-      -new \
-      -nodes \
-      -x509 \
-      -days 365 \
-      -newkey rsa:2048 \
-      -config /usr/src/app/cert.cnf \
-      -keyout privkey.pem \
-      -out cert.pem
+	touch privkey.pem
+	touch cert.pem
+	touch fullchain.pem
 
-    cat cert.pem privkey.pem > fullchain.pem
+	envsubst '${DOMAIN} ${ADMIN_EMAIL}' < /usr/src/app/cert.cnf.template > /usr/src/app/cert.cnf
+
+	cat /usr/src/app/cert.cnf
+
+	openssl req \
+		-new \
+		-nodes \
+		-x509 \
+		-days 365 \
+		-newkey rsa:2048 \
+		-config /usr/src/app/cert.cnf \
+		-keyout privkey.pem \
+		-out cert.pem
+
+	cat cert.pem privkey.pem > fullchain.pem
 fi
 
 #todo implement certbot
