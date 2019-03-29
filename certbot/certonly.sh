9b70a2f4cf0b8274ccbe0e5baab801f1b72ced3e
root <root@octopi.ml>
2019-03-29 17:48:46 +0000

    certbot fixes

--- certbot/certonly.sh
+++ certbot/certonly.sh
@@ -2,19 +2,28 @@
 
 set -e
 
-source ../.env
+PARENT_DIR=$(dirname "$(readlink -f "$0")")/../
+
+source ${PARENT_DIR}/.env
 
 docker run \
 	-it \
 	--rm \
-	--volume certbot-certs:/etc/letsencrypt \
-	--volume certbot-challenges:/data/letsencrypt \
+	--volume starter_certbot-certs:/etc/letsencrypt \
+	--volume starter_certbot-challenges:/data/letsencrypt \
 	certbot/certbot certonly \
 		--webroot \
 		--webroot-path /data/letsencrypt \
 		--agree-tos \
 		--non-interactive \
+		--cert-name ${DOMAIN} \
 		--email ${ADMIN_EMAIL} \
 		--domain ${DOMAIN} \
 		--domain www.${DOMAIN} \
-		--domain static.${DOMAIN}
+		--domain static.${DOMAIN} \
+		$( (("$DEBUG" == "1")) && echo "--dry-run" ) \
+		"$@"
+#		--post-hook "cd "$PARENT_DIR" && ls" #docker-compose kill -s SIGHUP nginx"
+
+
+cd $PARENT_DIR && docker-compose kill -s SIGHUP nginx
